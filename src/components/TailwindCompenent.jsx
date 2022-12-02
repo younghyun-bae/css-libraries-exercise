9f6d7dc75ecb5f45ba1ac913afbe924667c82605
Younghyun <yhbae97@gmail.com>
2022-12-02 22:30:44 +0100

    init project css libraries

---
+++ src/components/TailwindCompenent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function TailwindCompenent() {
+  return (
+  <div>
+    <h1 className='text-8xl'>Hello!</h1>
+    <button className='bg-blue-500 rounded-xl px-2'>Button</button>
+  </div>
+  );
+}
+
